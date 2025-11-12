d1dbd8f137d3875f5adbfc9d97c9f6c464521aa4
Prolay Halder <prolay@itobuz.com>
2025-11-12 14:49:25 +0530

    WMDT-66 Warehouse UI in progress

--- src/js/pages/dashboard/dashboard.js
+++ src/js/pages/dashboard/dashboard.js
@@ -4,24 +4,9 @@ import '../../../scss/dashboard.scss';
 import * as bootstrap from 'bootstrap';
 
 import dashboardSelection from './dashboardSelector';
-import {
-  addManagerSubscribe,
-  addWarehouseSubscribe,
-  showManagerOptions,
-} from './adminSubscribe.js';
+import { addManagerSubscribe } from './adminSubscribe.js';
 
 dashboardSelection.addManagerForm.addEventListener(
   'submit',
   addManagerSubscribe
-);
-
-dashboardSelection.addWarehouseForm.addEventListener(
-  'submit',
-  addWarehouseSubscribe
-);
-
-// Get all managers when Add-Warehouse button triggered
-dashboardSelection.addWarehouseButton.addEventListener(
-  'click',
-  showManagerOptions
 );
