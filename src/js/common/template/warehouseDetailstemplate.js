a70a0f913cd35edf19f2db70c90359f6745d91ef
Sanket Ganguly <sanket@itobuz.com>
2025-11-24 18:08:34 +0530

    WMDT-111 feat: warehouse capacity label in warehouse page and manager profile page

--- src/js/common/template/warehouseDetailstemplate.js
+++ src/js/common/template/warehouseDetailstemplate.js
@@ -1,4 +1,4 @@
-const addWarehouseDetails = (warehouse) => {
+const addWarehouseDetails = (warehouse, text) => {
   return `<div class="warehouse-card card mt-3" style="height: auto">
             <div class="card-body mb-0" id="${warehouse._id}">
               <h5 class="card-title warehouse-name mt-0">${warehouse.name}</h5>
@@ -7,6 +7,9 @@ const addWarehouseDetails = (warehouse) => {
               </p>
               <p class="card-text warehouse-detail">
                 <i class="fa-solid fa-location-dot mx-2"></i>${warehouse.address}
+              </p>
+              <p class="card-text warehouse-detail">
+                Stock Level: <span class="storage p-1 rounded ${text.toLowerCase()}">${text}</span>
               </p>
               <a href="/pages/products.html?warehouseId=${warehouse._id}&filter=warehouses" class="btn btn-primary warehouse-button">View Products</a>
             </div>
