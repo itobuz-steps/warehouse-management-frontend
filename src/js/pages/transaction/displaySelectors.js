9f01585c938c7e6522cfc984a881e2c55857c701
Prolay Halder <prolay@itobuz.com>
2025-11-25 11:30:11 +0530

    WMDT93 - fix : display selectors fixed

--- src/js/pages/transaction/displaySelectors.js
+++ src/js/pages/transaction/displaySelectors.js
@@ -1,13 +1,16 @@
+// js/pages/transaction/displaySelectors.js
 import {
   loadWarehouses,
   loadDestinationWarehouse,
   getWarehouses,
 } from './loadWarehouses.js';
 import { displayProducts } from './displayProducts.js';
+import { transactionSelectors } from './transactionSelector';
 
 const transactionTypeSelector = document.getElementById('transactionType');
 const warehouseDropdown = document.getElementById('warehouseDropdown');
 const transferOption = document.getElementById('transferOption');
+
 const sourceWarehouseDropdownLabel = document.getElementById(
   'sourceWarehouseDropdownLabel'
 );
@@ -19,12 +22,32 @@ const destinationWarehouseSelector = document.getElementById(
   'destinationWarehouse'
 );
 
+const { sections } = transactionSelectors;
+
+function hideAllSections() {
+  Object.values(sections).forEach((s) => s.classList.add('d-none'));
+}
+
+function resetWarehouseUI() {
+  warehouseDropdown.classList.add('d-none');
+
+  sourceWarehouseDropdownLabel.classList.add('d-none');
+  destinationWarehouseDropdownLabel.classList.add('d-none');
+  sourceWarehouseSelector.classList.add('d-none');
+  destinationWarehouseSelector.classList.add('d-none');
+
+  // avoid stacking listeners
+  sourceWarehouseSelector.onchange = null;
+  destinationWarehouseSelector.onchange = null;
+}
+
 export async function displayTransactionType() {
   try {
     const assignedWarehouses = await getWarehouses();
-
-    if (assignedWarehouses.length < 2) {
+    if (!assignedWarehouses || assignedWarehouses.length < 2) {
       transferOption.setAttribute('disabled', '');
+    } else {
+      transferOption.removeAttribute('disabled');
     }
   } catch (err) {
     console.log(err.message);
@@ -32,43 +55,52 @@ export async function displayTransactionType() {
 }
 
 export function displayWarehouseDropdown() {
+  const type = transactionTypeSelector.value;
+
+  hideAllSections();
+  resetWarehouseUI();
+
+  if (!type) return;
+
   warehouseDropdown.classList.remove('d-none');
 
-  sourceWarehouseDropdownLabel.classList.add('d-none');
-  destinationWarehouseDropdownLabel.classList.add('d-none');
-  sourceWarehouseSelector.classList.add('d-none');
-  destinationWarehouseSelector.classList.add('d-none');
+  if (type === 'IN') {
+    // Only destination warehouse matters
+    destinationWarehouseDropdownLabel.classList.remove('d-none');
+    destinationWarehouseSelector.classList.remove('d-none');
 
-  if (transactionTypeSelector.value === 'IN') {
-    // stock in -- work with destination warehouse
+    destinationWarehouseSelector.onchange = () => displayProducts('IN');
+    sections.IN.classList.remove('d-none');
+  } else if (type === 'OUT') {
+    // Only source warehouse
+    sourceWarehouseDropdownLabel.classList.remove('d-none');
+    sourceWarehouseSelector.classList.remove('d-none');
+
+    sourceWarehouseSelector.onchange = () => displayProducts('OUT');
+    sections.OUT.classList.remove('d-none');
+  } else if (type === 'ADJUSTMENT') {
+    // Only source warehouse
+    sourceWarehouseDropdownLabel.classList.remove('d-none');
+    sourceWarehouseSelector.classList.remove('d-none');
+
+    sourceWarehouseSelector.onchange = () => displayProducts('ADJUSTMENT');
+    sections.ADJUSTMENT.classList.remove('d-none');
+  } else if (type === 'TRANSFER') {
+    // source + destination
+    sourceWarehouseDropdownLabel.classList.remove('d-none');
+    sourceWarehouseSelector.classList.remove('d-none');
+
+    sourceWarehouseSelector.onchange = async () => {
+      await loadDestinationWarehouse();
+      await displayProducts('TRANSFER');
+    };
 
     destinationWarehouseDropdownLabel.classList.remove('d-none');
     destinationWarehouseSelector.classList.remove('d-none');
 
-    destinationWarehouseSelector.addEventListener('change', () => {
-      displayProducts('IN');
-    });
-  } else if (transactionTypeSelector.value === 'OUT') {
-    // stock out -- work with source warehouse
-
-    sourceWarehouseDropdownLabel.classList.remove('d-none');
-    sourceWarehouseSelector.classList.remove('d-none');
-  } else if (transactionTypeSelector.value === 'ADJUSTMENT') {
-    // stock adjustment -- work with source warehouse
-
-    sourceWarehouseDropdownLabel.classList.remove('d-none');
-    sourceWarehouseSelector.classList.remove('d-none');
-  } else {
-    // transfer stock -- use both source and destination warehouse
-
-    sourceWarehouseDropdownLabel.classList.remove('d-none');
-    sourceWarehouseSelector.classList.remove('d-none');
-
-    sourceWarehouseSelector.addEventListener(
-      'change',
-      loadDestinationWarehouse
-    );
+    sections.TRANSFER.classList.remove('d-none');
   }
 
+  // (Re)fill options whenever type changes
   loadWarehouses();
 }
