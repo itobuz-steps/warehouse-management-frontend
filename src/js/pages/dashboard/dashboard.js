dd1566945d5bf6f5477698cd45d87fe4926d0141
Sanket Ganguly <sanket@itobuz.com>
2025-11-28 18:39:52 +0530

    WMDT-123 feat: export to excel file with dummy data

--- src/js/pages/dashboard/dashboard.js
+++ src/js/pages/dashboard/dashboard.js
@@ -2,6 +2,7 @@ import '../../../scss/styles.scss';
 import '../../../scss/dashboard.scss';
 // eslint-disable-next-line no-unused-vars
 import * as bootstrap from 'bootstrap';
+import * as XLSX from 'xlsx';
 
 import dashboardSelection from './dashboardSelector';
 import { addManagerSubscribe } from './adminSubscribe.js';
@@ -20,7 +21,6 @@ dashboardSelection.addManagerForm.addEventListener(
   addManagerSubscribe
 );
 
-
 document.addEventListener('DOMContentLoaded', async () => {
   await fetchUserAndWarehouses(dashboardSelection.warehouseSelect);
   const firstWarehouse = dashboardSelection.warehouseSelect.value;
@@ -38,4 +38,40 @@ dashboardSelection.warehouseSelect.addEventListener('change', async () => {
   await showProductTransactionSubscribe(selectedWarehouseId);
   await showTransactionStatsSubscribe(selectedWarehouseId);
   await showLowStockProducts(selectedWarehouseId);
+});
+
+document.getElementById('export').addEventListener('click', (event) => {
+  const jsonString = event.target.getAttribute('data-json');
+  if (!jsonString) {
+    return;
+  }
+
+  let data;
+
+  try {
+    data = JSON.parse(jsonString);
+  } catch (err) {
+    console.error('Invalid JSON:', err);
+    return;
+  }
+  if (!Array.isArray(data) || data.length === 0) {
+    return;
+  }
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+  const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'exported-data.xlsx';
+  document.body.appendChild(a);
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
 });
