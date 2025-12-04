6bf6948d6acceb2a994694b13175a89beb5a902d
Prolay Halder <prolay@itobuz.com>
2025-12-04 16:01:19 +0530

    WMDT143 - fix: managers card UI

--- src/js/common/template/profileTemplate.js
+++ src/js/common/template/profileTemplate.js
@@ -1,23 +1,32 @@
 const verifiedManagerCard = (
   name,
   email,
-  profileImage = '../../assets/images/profile_default.svg'
+  profileImage = '../../assets/images/profile_default.svg',
+  lastLogin
 ) => {
   return `<div class="manager-card">
-            <img
-              src=${profileImage}
-              alt="Manager"
-              class="manager-avatar"
-            />
-            <div class="manager-info">
-              <span>
-                <h5 class="manager-name d-inline-block">${name}</h5>
-                <i class="fas fa-check-circle fs-6 verified"></i>
-              </span>
+  <img src="${profileImage}" alt="Manager" class="manager-avatar" />
 
-              <p class="manager-email">${email}</p>
-            </div>
-          </div>`;
+  <div class="manager-info">
+    <div class="manager-header">
+      <h5 class="manager-name">${name}</h5>
+      <i class="fas fa-check-circle verified"></i>
+    </div>
+
+    <p class="manager-email">${email}</p>
+
+    <!-- NEW: Last Login -->
+    <p class="manager-last-login">
+      <i class="far fa-clock"></i> Last Login: ${lastLogin}
+    </p>
+
+    <!-- NEW: Block User Button -->
+    <button class="block-user-btn">
+      <i class="fas fa-ban"></i> Block User
+    </button>
+  </div>
+</div>
+`;
 };
 
 const unverifiedManagerCard = (
