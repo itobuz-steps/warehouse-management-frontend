62e7ca7c18a4a7dd55d508a82ba603428ce25fac
Devjyoti Banerjee <devjyoti@itobuz.com>
2026-01-06 11:30:24 +0530

    WMDT-183 fix: user profile warehouse cards styling

--- src/js/common/template/warehouseDetailstemplate.js
+++ src/js/common/template/warehouseDetailstemplate.js
@@ -2,17 +2,17 @@ const addWarehouseDetails = (warehouse, text) => {
   return `
   <div class="warehouse-card card mt-3 h-100 d-flex flex-column">
   <div class="card-body flex-grow-1">
-    <h5 class="card-title warehouse-name mb-3">${warehouse.name}</h5>
+    <h5 class="card-title warehouse-name mb-1">${warehouse.name}</h5>
 
-    <div class="warehouse-detail scrollable-description mb-3">
-      <i class="fa-solid fa-circle-info text-muted me-2"></i>
-      <span class="description-text">${warehouse.description}</span>
-    </div>
-
-    <p class="warehouse-detail mb-2">
+    <p class="warehouse-detail mb-1">
       <i class="fa-solid fa-location-dot text-danger me-2"></i>
       <span class="location-text">${warehouse.address}</span>
     </p>
+
+    <div class="warehouse-detail scrollable-description mb-2">
+      <i class="fa-solid fa-circle-info text-muted me-2 align-top"></i>
+      <span class="description-text">${warehouse.description}</span>
+    </div>
 
     <div class="warehouse-detail mt-2">
       <span class="me-2">Stock Level:</span>
@@ -21,7 +21,7 @@ const addWarehouseDetails = (warehouse, text) => {
       </span>
     </div>
 
-    <div class="mt-auto pt-3">
+    <div class="mt-auto pt-1">
       <a
         href="/pages/products.html?warehouseId=${warehouse._id}&filter=warehouses"
         class="btn warehouse-button w-100"
